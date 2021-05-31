1252f7cc6d343cc14afb240583a0b31a7d649d16
sanbo <sanbo.xyz@gmail.com>
2021-05-31 17:09:36 +0800

    update log

--- app/src/main/java/jianqiang/com/hook3/hook/AMSHookHelper.java
+++ app/src/main/java/jianqiang/com/hook3/hook/AMSHookHelper.java
@@ -34,9 +34,11 @@ public class AMSHookHelper {
                 //获取ActivityManager的单例IActivityManagerSingleton，他其实就是之前的gDefault
                 gDefault = RefInvoke.getStaticFieldObject("android.app.ActivityManager", "IActivityManagerSingleton");
             }
+            logi("hookAMN gDefault: " + gDefault);
 
             // gDefault是一个 android.util.Singleton<T>对象; 我们取出这个单例里面的mInstance字段
             Object mInstance = RefInvoke.getFieldObject("android.util.Singleton", gDefault, "mInstance");
+            logi("hookAMN mInstance: " + mInstance);
 
             // 创建一个这个对象的代理对象MockClass1, 然后替换这个字段, 让我们的代理对象帮忙干活
             Class<?> classB2Interface = Class.forName("android.app.IActivityManager");
@@ -44,6 +46,7 @@ public class AMSHookHelper {
                     Thread.currentThread().getContextClassLoader(),
                     new Class<?>[]{classB2Interface},
                     new MockClass1(mInstance));
+            logi("hookAMN proxy: " + proxy);
 
             //把gDefault的mInstance字段，修改为proxy
             Class class1 = gDefault.getClass();
@@ -72,10 +75,11 @@ public class AMSHookHelper {
         try {
             // 先获取到当前的ActivityThread对象
             Object currentActivityThread = RefInvoke.getStaticFieldObject("android.app.ActivityThread", "sCurrentActivityThread");
+            logi("hookActivityThread currentActivityThread: " + currentActivityThread);
 
             // 由于ActivityThread一个进程只有一个,我们获取这个对象的mH
             Handler mH = (Handler) RefInvoke.getFieldObject(currentActivityThread, "mH");
-
+            logi("hookActivityThread mH: " + mH);
 
             //把Handler的mCallback字段，替换为new MockClass2(mH)
             RefInvoke.setFieldObject(Handler.class, mH, "mCallback", new MockClass2(mH));
